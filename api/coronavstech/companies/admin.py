a17382b72a5db2174ace15d4d55f9a0f63ed8745
devcrack <aurelio.hdz@yandex.com>
2023-03-08 00:20:11 -0600

    Re Added the entire project?

--- api/coronavstech/companies/admin.py
+++ api/coronavstech/companies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+# WAY #1
+# class CompanyAdmin(admin.ModelAdmin):
+#     model = Company
+#
+#
+# admin.site.register(Company, CompanyAdmin)
+
+
+# WAY 2
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    model = Company
