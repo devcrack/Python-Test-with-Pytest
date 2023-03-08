f348c5c2a941c5ff4216f08ebc5578f02755360b
devcrack <aurelio.hdz@yandex.com>
2023-03-08 01:07:51 -0600

    Reformated Django Rest Framework API code

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+# Local
+from .serializers import CompanySerializer
+from .models import Company
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_update")
